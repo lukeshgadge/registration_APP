1f6cee7a5403d3cf3b2a611d33cc16798e0c21cf
lukeshgadge <lukeshgadge89@gmailcom>
2024-12-30 16:33:15 +0530

    add sg

--- base_infra/main.tf
+++ base_infra/main.tf
@@ -73,6 +73,22 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 9003
+    to_port     = 9003
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
